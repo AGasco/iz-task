347fd585d12ccb01eacc32c0a73a709725e254c0
Antonio Gasco <antoniofgasco@gmail.com>
2022-10-24 17:38:04 +0100

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header';
+import ProductsListPage from './pages/ProductsListPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
 import './App.scss';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/products" element={<ProductsListPage />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
+        <Route path="*" element={<Navigate to="/products" replace />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
