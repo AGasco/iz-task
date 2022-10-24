f337225f7991632bfe8bf3f8d12f599c041009d7
Antonio Gasco <antoniofgasco@gmail.com>
2022-10-24 17:53:58 +0100

    Header - prototype renders and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import LogoZara from '../icons/logo-zara.png';
+import styles from '../styles/Header.module.scss';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.Header}>
+      <div className={styles.Header__Content}>
+        <img src={LogoZara} alt="ZARA's Logo" />
+        <div className={styles.Header__Cart}>Cart: 0</div>
+      </div>
+      <div className={styles.Header__Breadcrumbs}>
+        breadcrumbs > breadcrumbs > breadcrumbs
+      </div>
+    </div>
+  );
 };
 
 export default Header;
